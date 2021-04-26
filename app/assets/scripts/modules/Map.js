d9c1af19daf741ff298167a434f5793d78bc9875
jagadeesh <jagadeesh.kumar2091@gmail.com>
2021-04-27 00:23:51 +0530

    Updated Map.js to add circle and move with keyboard

--- app/assets/scripts/modules/Map.js
+++ app/assets/scripts/modules/Map.js
@@ -2,23 +2,54 @@ import L from "leaflet";
 
 class Map {
   constructor() {
-    this.map = document.getElementById("map");
-    this.render();
+    this.mapContainer = document.getElementById("map");
+    this.lat = 12.9716;
+    this.long = 77.5946;
+
     this.events();
+    this.addMap();
+    this.render();
+  }
+
+  addCircle() {
+    this.circle = L.circle([this.lat, this.long], {
+      fillColor: "#f00",
+      radius: 50,
+      color: "f00",
+      fillOpacity: 1,
+    });
+    return this.circle;
+  }
+
+  addMap() {
+    this.map = L.map(this.mapContainer);
   }
 
   render() {
-    const map = L.map(this.map);
-    map.setView([51.505, -0.09], 13);
+    // const map = L.map(this.mapContainer);
+    this.map.setView([this.lat, this.long], 13);
     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
         '&copy; <a href="https://openstreetmap.org/copyright">OpenStreepMap Contributors</a>',
-      tileSize: 128,
-    }).addTo(map);
+    }).addTo(this.map);
+    this.addCircle().addTo(this.map);
+  }
+
+  addNewCircle() {
+    let paths = document.getElementsByClassName("leaflet-interactive");
+    paths.forEach((p) => p.remove());
+    this.map.setView([this.lat, this.long], 13);
+    this.addCircle().addTo(this.map);
   }
 
   events() {
-    this.map.addEventListener("click", (e) => console.log(e));
+    window.addEventListener("keyup", (e) => {
+      if (e.key === "ArrowUp") {
+        this.lat += 0.001;
+        console.log(this.lat);
+        this.addNewCircle();
+      }
+    });
   }
 }
 
