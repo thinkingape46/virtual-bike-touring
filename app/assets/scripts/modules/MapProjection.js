3578bc9ea25af50b7fbc8d3036e47aed681f00c0
jagadeesh <jagadeesh.kumar2091@gmail.com>
2021-04-30 23:57:58 +0530

    Updated MapProjection.js, able to implement latitude and longitude

--- app/assets/scripts/modules/MapProjection.js
+++ app/assets/scripts/modules/MapProjection.js
@@ -4,9 +4,19 @@ class MapProjection {
     this.events();
   }
 
+  getLat(offsetX, clientWidth) {
+    return (offsetX / clientWidth - 0.5) * 360;
+  }
+
   events() {
     this.projectionDiv.addEventListener("click", (e) => {
-      console.log(e);
+      console.log(this.getLat(e.offsetX, e.target.clientWidth));
+
+      console.log(
+        ((e.target.clientHeight / 2 - e.offsetY) /
+          (e.target.clientHeight / 2)) *
+          90
+      );
     });
   }
 }
